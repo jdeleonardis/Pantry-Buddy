5a015376bf83b3eb7a677c402ac12b295db82b12
Michael P Nance <michael.p.nance@gmail.com>
2020-04-21 18:20:02 -0400

    Added api call functions.  Added function to convert ingredients list from the DOM to a comma delimited string.

--- script/edamam.js
+++ script/edamam.js
@@ -4,7 +4,6 @@
     const spoonPath = "https://api.spoonacular.com/recipes/"
     const spoonKey = "422896595733422cab793b743632e7d6";
 
-    const ingredients = "chicken, rice, cheese";
 
     // Queries Spoonacular API to get a list of up to n
     // recipes matching given string of ingredients.
@@ -40,9 +39,30 @@
         });
     }
 
-    //get list of recipes
-    const recList = await searchRecipesbyIngredients(5, ingredients);
-    const newList = await Promise.all(recList.map(recipe => getRecipeInfo(recipe)));
-    console.log(newList);
+    //Returns list of ingredients represented as comma delimited string
+    function getIngredientsString() {
+        return $.map($("#ingredientUL").children(), (item) => $(item).text().slice(0, -1)).toString();
+    }
+    $("#searchBtn").click(async() => {
+        console.log(await getRecipes(5, getIngredientsString()));
+    });
 
+    // Returns an array of up to n recipes based on given ingredients
+    async function getRecipes(n, ingredients) {
+        const recList = await searchRecipesbyIngredients(n, ingredients);
+        const newList = await Promise.all(recList.map(recipe => getRecipeInfo(recipe)));
+        return newList;
+    }
+
+    function renderRecipes(recipes) {
+        $("#fiveRecipes").empty();
+        recipes.forEach(recipe => {
+            let card = $("<div>").addClass("column sectionContent recipediv")
+                .append($("<p>").text(recipe.title))
+                .append($("<img>").attr("src", recipe.image))
+                .append($("<p>").text("Calories: " + recipe.nutrition.nutrients[0].amount));
+            $("#fiveRecipes").append(card);
+        });
+
+    }
 })();
